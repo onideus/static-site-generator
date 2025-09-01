3b6b412c13ef34a503c6483adc9395f7cae2e637
Zach Martin <zach@zach-martin.com>
2025-09-01 00:03:22 -0400

    further tests for parent node

--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -18,3 +18,23 @@ class TestParentNode(unittest.TestCase):
             parent_node.to_html(),
             "<div><span><b>grandchild</b></span></div>",
         )
+
+    def test_to_html_raises_without_tag(self):
+        child_node = LeafNode("span", "child")
+        with self.assertRaises(ValueError):
+            ParentNode(None, [child_node]).to_html()
+
+    def test_to_html_raises_without_children(self):
+        with self.assertRaises(ValueError):
+            ParentNode("div", None).to_html()
+
+    def test_to_html_with_props_on_parent(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node], {"class": "wrapper", "id": "root"})
+        self.assertEqual(parent_node.to_html(), '<div class="wrapper" id="root"><span>child</span></div>')
+
+    def test_to_html_with_multiple_children(self):
+        child1 = LeafNode("span", "first")
+        child2 = LeafNode("b", "second")
+        parent_node = ParentNode("div", [child1, child2])
+        self.assertEqual(parent_node.to_html(), "<div><span>first</span><b>second</b></div>")
